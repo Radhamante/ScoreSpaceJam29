ecbe7a1a6cfe995a53a07e36054d96288d37aada
LeaHertyl <lea.brechet@yahoo.fr>
2024-04-20 02:59:41 +0200

    feat : player look at mouse

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+@export var camera : Camera3D
 
 var mouse_left_down: bool = false
 func _input( event ):
@@ -8,11 +9,32 @@ func _input( event ):
 			mouse_left_down = true
 		elif event.button_index == 1 and not event.is_pressed():
 			mouse_left_down = false
+			
+func get_mouse_position():
+	var mouse_position = get_viewport().get_mouse_position()
+	
+	var spaceState = get_world_3d().direct_space_state
+	var rayOrigin = camera.project_ray_origin(mouse_position)
+	var rayEnd = rayOrigin + camera.project_ray_normal(mouse_position) * 2000
+	
+	var rayQuery = PhysicsRayQueryParameters3D.new()
+	rayQuery.from = rayOrigin
+	rayQuery.to = rayEnd
+	
+	var rayArray = spaceState.intersect_ray(rayQuery)
+	
+	if rayArray.has("position"):
+		return rayArray["position"]
+
 func _process( some_change ):
 	if mouse_left_down:
-		var mouse_position = get_viewport().get_mouse_position()
-		print(mouse_position)
-		print(position)
+		var mousePos = get_mouse_position()
+		if mousePos != null:
+			look_at(mousePos, Vector3.UP)
+			rotation.x = 0
+		
+		#print(mouse_position)
+		#print(position)
 
 const SPEED = 5.0
 
