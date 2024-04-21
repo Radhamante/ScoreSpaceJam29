749f66fd1be6d25531e47e14f2d1c6b6faad25ff
Samsagas <116380177+Samsagas@users.noreply.github.com>
2024-04-21 21:15:09 +0200

    feat : end game

---
+++ Global/Preloads.gd
@@ -0,0 +1,16 @@
+extends Node
+
+const mainmenu_scene = preload("res://Menu/MainMenu/MainMenu.tscn")
+const leaderboard_scene = preload("res://Menu/Leaderboard/Leaderboard.tscn")
+const credit_scene = preload("res://Menu/Credits/Credits.tscn")
+const setting_scene = preload("res://Menu/Settings/Settings.tscn")
+const level_scene = preload("res://Levels/Level1.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
